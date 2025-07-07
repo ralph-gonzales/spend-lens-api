956b11ac3f76596b8ff54e2799a42d65a9e80c70
ralph gonzales <rmpgonzales@gmail.com>
2025-07-07 13:42:57 +0800

    feat: add User entity class

---
+++ src/main/java/dev/ralphgonzales/spendlens/entity/User.java
@@ -0,0 +1,20 @@
+package dev.ralphgonzales.spendlens.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String contactNumber;
+    private String roleTypeCode;
+}
