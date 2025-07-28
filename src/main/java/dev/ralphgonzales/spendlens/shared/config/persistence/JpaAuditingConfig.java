7a8d706fd5578c77bb00752d82427899da0d1485
ralph gonzales <rmpgonzales@gmail.com>
2025-07-28 17:32:49 +0800

    feat(core): add configuration for validation parameters and update config package structure
    Introduce ValidationConfig and ValidationProperties to centralize validation-related settings and reorganize configuration classes.
    
    - Add ValidationConfig to manage all validation-related configurations.
    - Implement ValidationProperties to bind validation message parameters from application.yml
    - Move JpaAuditingConfig to persistence package to group related configuration classes

--- src/main/java/dev/ralphgonzales/spendlens/shared/config/persistence/JpaAuditingConfig.java
+++ src/main/java/dev/ralphgonzales/spendlens/shared/config/persistence/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package dev.ralphgonzales.spendlens.shared.config;
+package dev.ralphgonzales.spendlens.shared.config.persistence;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
---
+++ src/main/java/dev/ralphgonzales/spendlens/shared/config/validation/ValidationConfig.java
@@ -0,0 +1,9 @@
+package dev.ralphgonzales.spendlens.shared.config.validation;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+public class ValidationConfig {
+}
